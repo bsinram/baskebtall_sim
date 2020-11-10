360eb4eba5d42a69a92367e426d34a1a23c7302f
Brandon Work <Work@Brandons-MacBook-Pro.local>
2020-11-10 10:35:43 -0500

    added kobe

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ if __name__ == '__main__':
   # Set self.human to "False" for both players to simulate
   p1 = Player("LeBron James", True, False, 65, 90, 20, 10)
   p2 = Player("Michael Jordan", False, False, 60, 90, 15, 20)
+  p3 = Player("Kobe Bryant", False, False, 60, 90, 15, 20)
+
 
   g = Game(p1, p2)
   g.run_game()
